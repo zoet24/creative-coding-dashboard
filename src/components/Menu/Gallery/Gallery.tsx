1616ebeb6dbcf73692ff519745d3449303925b31
Zoe Thexton <zoe.thexton@anya.health>
2025-06-05 21:45:24 +0100

    Add search functionality

--- src/components/Menu/Gallery/Gallery.tsx
+++ src/components/Menu/Gallery/Gallery.tsx
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 import { useActiveProject } from "../../../context/ActiveProjectContext";
 import { loadProject } from "../../../lib/loadProject";
 import { loadProjects } from "../../../lib/loadProjects";
-import { ProjectConfig, projectCategories } from "../../../lib/types";
+import {
+  ProjectCategory,
+  ProjectConfig,
+  projectCategories,
+} from "../../../lib/types";
 import ProjectCard from "../../ProjectCard/ProjectCard";
 import { Input } from "../../ui/input";
 
 const Gallery = () => {
   const [projects, setProjects] = useState<ProjectConfig[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState<
+    ProjectCategory | "All"
+  >("All");
   const { setProject, config: activeConfig } = useActiveProject();
 
   useEffect(() => {
@@ -25,11 +33,37 @@ const Gallery = () => {
     }
   };
 
+  const filteredProjects = projects.filter((project) => {
+    const matchesSearchTitle = project.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    const matchesSearchDescription = project.description
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    const matchesCategory =
+      selectedCategory === "All" ||
+      project.categories.includes(selectedCategory);
+
+    return (matchesSearchTitle || matchesSearchDescription) && matchesCategory;
+  });
+
   return (
     <div className="p-4 space-y-4">
       <div className="flex gap-2">
-        <Input placeholder="Search..." />
-        <select className="border px-2 py-1 rounded-md">
+        <Input
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <select
+          className="border px-2 py-1 rounded-md"
+          value={selectedCategory}
+          onChange={(e) =>
+            setSelectedCategory(e.target.value as ProjectCategory | "All")
+          }
+        >
           <option value="All">All</option>
           {projectCategories.map((category) => (
             <option key={category} value={category}>
@@ -38,7 +72,7 @@ const Gallery = () => {
           ))}
         </select>
       </div>
-      {projects.map((project, i) => (
+      {filteredProjects.map((project, i) => (
         <ProjectCard
           key={i}
           project={project}
@@ -46,6 +80,10 @@ const Gallery = () => {
           onClick={() => handleProjectClick(project)}
         />
       ))}
+
+      {filteredProjects.length === 0 && (
+        <div className="text-sm text-gray-500">No projects found.</div>
+      )}
     </div>
   );
 };
